9a5cb2afd2eb28e6f374957eb9c868914c94a616
moroz <morozarturv@gmail.com>
2014-10-06 16:52:35 +0300

    all pages

--- js/common.js
+++ js/common.js
@@ -1,8 +1,30 @@
 head.ready(function() {
 
-	// $(document).on("click", function(){
-	// 	$(".js-popup").hide();
-	// });
+	// masonry
+	var gallery = $('.js-gallery'),
+			body = $('body'),
+			item = gallery.find('.gallery__item');
+	gallery.isotope({
+		itemSelector: '.gallery__item',
+		masonry: {
+		  columnWidth: 200,
+		  gutter: 20
+		}
+	});
+	item.fancybox({
+		openEffect: 'fade',
+		closeEffect: 'fade',
+		nextEffect: 'none',
+		prevEffect: 'none',
+		padding: [20, 20, 55, 20],
+		margin: 30,
+		afterShow: function () {
+			var customContent = "<button class='fancybox-menu'></button>"
+			$('.fancybox-outer').append(customContent);
+		}
+	});
+	body.on('click', '.fancybox-menu', function () {
+		$('.fancybox-close').trigger('click');
+	});
 
-	console.log($('body').html());
 });
