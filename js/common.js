194cc5e1c7342979c6bec4f1015b9ce1c46e4270
moroz <morozarturv@gmail.com>
2014-10-16 12:55:22 +0300

    upd fixes

--- js/common.js
+++ js/common.js
@@ -4,7 +4,8 @@ head.ready(function() {
 	var gallery = $('.js-gallery'),
 			body = $('body'),
 			item = gallery.find('.gallery__item'),
-			up = $('.up');
+			up = $('.up'),
+			content = $('.js-content');
 	gallery.isotope({
 		itemSelector: '.gallery__item',
 		masonry: {
@@ -29,6 +30,19 @@ head.ready(function() {
 	});
 	up.on('click', function () {
 		$('.gallery').animate({scrollTop: 0}, 500);
+	});
+	function content_pic () {
+		if (content.length) {
+			var content_in = content.find('.content__in'),
+					content_pic = content.find('.content__pic'),
+					content_height = content.height(),
+					content_in_height = content_in.outerHeight();
+			content_pic.height(content_height - content_in_height);
+		};
+	}
+	content_pic();
+	$(window).resize(function () {
+		content_pic();
 	});
 
 });
